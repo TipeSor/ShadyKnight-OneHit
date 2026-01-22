a451f5aaf605de7a0cdcc9f36ebf2dfdf0bd695e
TipeSor <antanas8899@gmail.com>
2026-01-22 23:58:16 +0200

    init

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace OneHit
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.NAME, PluginInfo.VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+
+        public void Awake()
+        {
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {PluginInfo.GUID} is loaded!");
+
+            Harmony harmony = new(PluginInfo.GUID);
+
+            PlayerController.OnDamage += static (_) => Game.instance.Restart();
+
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace OneHit
+{
+    internal static class PluginInfo
+    {
+        public const string NAME = "One Hit";
+        public const string GUID = "tipe.shadyknight.onehit";
+        public const string VERSION = "0.0.0";
+    }
+}
